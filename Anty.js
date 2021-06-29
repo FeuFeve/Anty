67e340e8463fe11a550d36e564ebdcb8b80c7e1c
FeuFeve <clement-potin@outlook.com>
2021-06-29 20:22:32 +0200

    0.3 (Auto ressources gathering)

--- Anty.js
+++ Anty.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Anty
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Fourmizzz/Antzzz helper
 // @author       FeuFeve
-// @match        http://s1.antzzz.org/alliance.php?Membres
+// @match        http://s1.antzzz.org/*
 // @icon         https://www.google.com/s2/favicons?domain=antzzz.org
 // @grant        none
 // ==/UserScript==
@@ -24,7 +24,19 @@
 
 
 
+    /* ENUMS */
+
+    const contexts = {
+        CROPS: "crops",
+    	MEMBERS: "members",
+        NONE: "none"
+    }
+
+
+
     /* GLOBAL VARIABLES */
+
+    let context
 
     let currentPlayer
     let playerList = []
@@ -45,6 +57,31 @@
 
     /* GLOBAL FUNCTIONS */
 
+    function updateContext() {
+        if (document.URL.includes("Ressources.php")) {
+            context = contexts.CROPS
+        }
+        else if (document.URL.includes("alliance.php?Membres")) {
+            context = contexts.MEMBERS
+        }
+        else {
+            context = contexts.NONE
+        }
+    }
+
+    function enhancePage() {
+        switch (context) {
+            case contexts.NONE:
+                break
+            case contexts.CROPS:
+                enhanceCropsPage()
+                break
+            case contexts.MEMBERS:
+                enhanceMembersPage()
+                break
+        }
+    }
+
     function stringToInt(str) {
         return parseInt(str.replaceAll(' ', ''))
     }
@@ -52,24 +89,51 @@
     function playerIsInRange(from, to) {
         let fromHf = stringToInt(from.hf.innerText)
         let toHf = stringToInt(to.hf.innerText)
-        console.log(`Comparing ${fromHf} to ${toHf}`)
 
         return fromHf / 2 < toHf && fromHf * 3 > toHf ? true : false
     }
 
 
 
-    /* Members page */
+    /* Crops/Ressources page */
 
-    function UpdateMembersPage() {
-        ParseMembersPageData()
-        ApplyMembersPageVisuals()
-
-        console.log("UpdateMembersPage done.")
+    function enhanceCropsPage() {
+        console.log("Welcome to the crops page!")
+        maximiseMaterialsGatheringWorkers()
     }
 
-    function ParseMembersPageData() {
+    function maximiseMaterialsGatheringWorkers() {
+        let totalHf = stringToInt(document.querySelector("#boite_tdc tbody tr td strong").innerText.substring(2).split('cm')[0])
+        let foodGatherers = stringToInt(document.querySelector("#RecolteNourriture").value)
+        let woodGatherers = stringToInt(document.querySelector("#RecolteMateriaux").value)
 
+        if (foodGatherers + woodGatherers != totalHf) {
+            woodGatherers = totalHf - foodGatherers
+            document.querySelector("#RecolteMateriaux").value = woodGatherers
+            document.querySelector("#ChangeRessource").click()
+        }
+    }
+
+
+
+    /* Members/Membres page */
+
+    function enhanceMembersPage() {
+        // MutationObserver
+        const targetNode = document.getElementById('centre');
+        const config = { attributes: false, childList: true, subtree: true };
+        const observer = new MutationObserver(updateMembersPage);
+        observer.observe(targetNode, config);
+    }
+
+    function updateMembersPage() {
+        parseMembersPageData()
+        applyMembersPageVisuals()
+
+        console.log("updateMembersPage done.")
+    }
+
+    function parseMembersPageData() {
         let currentPlayerPseudo = document.querySelector("#boiteInfo .titre_colonne_cliquable .titre_ressource").text.split(" ")[1]
         let table = document.querySelector("#tabMembresAlliance tbody")
 
@@ -88,10 +152,9 @@
         }
     }
 
-    function ApplyMembersPageVisuals() {
+    function applyMembersPageVisuals() {
         Object.assign(currentPlayer.name.firstChild.style, blueStyle)
         playerList.forEach(player => {
-            //console.log(`${player.name.innerText} - ${player.hf.innerText}`)
             if (playerIsInRange(currentPlayer, player)) {
                 Object.assign(player.name.firstChild.style, redStyle)
             }
@@ -106,11 +169,8 @@
         'use strict'
 
         window.addEventListener('load', function () {
-            // MutationObserver
-            const targetNode = document.getElementById('centre');
-            const config = { attributes: false, childList: true, subtree: true };
-            const observer = new MutationObserver(UpdateMembersPage);
-            observer.observe(targetNode, config);
+            updateContext()
+            enhancePage()
         })
     })()
 }
